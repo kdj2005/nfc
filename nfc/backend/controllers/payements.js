4e2e4046cd02264d7c5e7c617b359ed013321fb3
kdj2005 <kdjirene@gmail.com>
2026-06-09 23:55:06 +0200

    Fix variable name from cartID to cartId

--- nfc/backend/controllers/payements.js
+++ nfc/backend/controllers/payements.js
@@ -27,13 +27,13 @@ async function finaliserPaiement(req,res){
     }
     setEtat("paiement en cours");
 
-    const {cartID}=req.body;
+    const {cartId}=req.body;
 
-    if(!cartID){
+    if(!cartId){
         setEtat("echec");
         return res.status(400).json({message:"ID de carte manquant", error:true, succes:false});
     }
-    let user=await User.findOne({cartID:cartID});
+    let user=await User.findOne({cartId:cartId});
     if(!user){
         setEtat("echec");
         return res.status(404).json({message:"Utilisateur non trouvé", error:true, succes:false});
